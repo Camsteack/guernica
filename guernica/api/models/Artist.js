3a05eb9b3e6e94a28374f43378bbcb5d5810eb11
Dinesh <dineshviswanath@gmail.com>
2015-09-26 15:59:01 +0100

    Initial api

---
+++ guernica/api/models/Artist.js
@@ -0,0 +1,18 @@
+/**
+* Artist.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+  	fisrtname : 'string',
+  	lastname : 'string',
+  	email : 'string',
+  	bio : 'string',
+  	country : 'string'
+  }
+};
+
