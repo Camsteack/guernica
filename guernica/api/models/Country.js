269cf3a013fe7a788ccee9d6161c19ee84bb6e58
Dinesh <dineshviswanath@gmail.com>
2015-09-26 18:06:15 +0100

    Add category and country api

--- guernica/api/models/Country.js
+++ guernica/api/models/Country.js
@@ -1,5 +1,5 @@
 /**
-* Artist.js
+* Country.js
 *
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
@@ -8,12 +8,7 @@
 module.exports = {
 
   attributes: {
-  	fisrtname : 'string',
-  	lastname : 'string',
-  	title : 'string',
-  	email : 'string',
-  	bio : 'string',
-  	country : 'string'
+  	name : 'string'
   }
 };
 
